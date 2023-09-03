b10bd30aa34d6491a538f0277b285bbcbfc2175e
Nazmul_Hossain <nazmulhossain2905@gmail.com>
2023-09-03 10:46:07 +0600

    generate rgb color

---
+++ app.js
@@ -0,0 +1,31 @@
+// change the background color by clicking the button. And every click should be random background color.
+
+// Step 1: Create onload handler
+
+window.onload = function () {
+  main();
+};
+
+function main() {
+  // Step 3: Getting important references
+  const btn = document.getElementById("btn");
+
+  // Step 4: Button click handler
+  btn.addEventListener("click", function (e) {
+    const bgColor = generateRGBColor();
+    document.body.style.backgroundColor = bgColor;
+  });
+}
+
+// Step 2: Generate a function that should be returned RGB color. Like rgb(255,255,255).
+function generateRGBColor() {
+  const red = Math.floor(Math.random() * 255);
+  const green = Math.floor(Math.random() * 255);
+  const blue = Math.floor(Math.random() * 255);
+
+  return `rgba(${red}, ${green}, ${blue})`;
+}
+
+// Step 3: Getting important references
+
+// Step 4: Button click handler
